26b97dc3fbb78c0b34480566a38aac106c8f8b94
Sclea3 <sashamaty30@gmail.com>
2026-01-04 19:44:55 +0400

    Практикуемся в работе с HTTPX. Практикуемся в работе с HTTPX: обновление пользователя

---
+++ httpx_update_user.py
@@ -0,0 +1,44 @@
+import httpx
+
+from tools.fakers import get_random_email
+
+
+client = httpx.Client(base_url="http://localhost:8000")
+
+payload = {
+    "email": get_random_email(),
+    "password": "password",
+    "lastName": "Ivanov",
+    "firstName": "Ivan",
+    "middleName": "Ivanovich"
+}
+
+create_user_response = client.post("/api/v1/users", json=payload)
+create_user_response.raise_for_status()
+
+login_payload = {
+    "email": payload["email"],
+    "password": payload["password"]
+}
+get_jwt_response = client.post("/api/v1/authentication/login", json=login_payload)
+get_jwt_response.raise_for_status()
+
+access_token = get_jwt_response.json()["token"]["accessToken"]
+headers = {
+    "Authorization": f"Bearer {access_token}"
+}
+
+patch_payload = {
+    "email": get_random_email(),
+    "lastName": "Petrov",
+    "firstName": "Petr",
+    "middleName": "Petrovich"
+}
+
+user_id = create_user_response.json()["user"]["id"]
+patch_response = client.patch(
+    f"/api/v1/users/{user_id}",
+    headers=headers,
+    json=patch_payload
+)
+patch_response.raise_for_status()
