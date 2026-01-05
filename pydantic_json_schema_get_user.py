0e7400172de0de63b00a7d9aa26979138e90498e
wynplex <wynplex@gmail.com>
2026-01-05 17:48:07 +0400

    Валидация JSON schema. Практикуемся в работе с JSON schema

---
+++ pydantic_json_schema_get_user.py
@@ -0,0 +1,29 @@
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.fakers import get_random_email
+
+
+public_users_client = get_public_users_client()
+
+create_user_request = CreateUserRequestSchema(
+	email=get_random_email(),
+    password="string",
+    last_name="string",
+    first_name="string",
+    middle_name="string"
+)
+
+create_user_response = public_users_client.create_user(create_user_request)
+
+authentication_user = AuthenticationUserSchema(
+	email = create_user_request.email,
+	password = create_user_request.password
+)
+private_api_client = get_private_users_client(authentication_user)
+
+get_user_schema = GetUserResponseSchema.model_json_schema()
+get_user_response = private_api_client.get_user_api(create_user_response.user.id)
+validate_json_schema(instance=get_user_response.json(), schema=get_user_schema)
