9e05859b5b11595d6ca397e6c9d41b7a05f63f18
wynplex <wynplex@gmail.com>
2026-01-07 23:18:12 +0400

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,39 +2,52 @@ import pytest
 from pydantic import BaseModel, EmailStr
 
 from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
-from clients.users.public_users_client import get_public_users_client, PublicUsersClient
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
+from clients.users.public_users_client import PublicUsersClient, get_public_users_client
 # Импортируем запрос и ответ создания пользователя, модель данных пользователя
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
 
 # Модель для агрегации возвращаемых данных фикстурой function_user
 class UserFixture(BaseModel):
-    request: CreateUserRequestSchema
-    response: CreateUserResponseSchema
+	request: CreateUserRequestSchema
+	response: CreateUserResponseSchema
+	authentication_user: AuthenticationUserSchema
 
-    @property
-    def email(self) -> EmailStr:  # Быстрый доступ к email пользователя
-        return self.request.email
+	@property
+	def email(self) -> EmailStr:  # Быстрый доступ к email пользователя
+		return self.request.email
 
-    @property
-    def password(self) -> str:  # Быстрый доступ к password пользователя
-        return self.request.password
+	@property
+	def password(self) -> str:  # Быстрый доступ к password пользователя
+		return self.request.password
 
 
 @pytest.fixture
 def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
+	return get_authentication_client()
 
 
 @pytest.fixture
 def public_users_client() -> PublicUsersClient:
-    return get_public_users_client()
+	return get_public_users_client()
 
 
-# Фикстура для создания пользователя
 @pytest.fixture
-# Используем фикстуру public_users_client, которая создает нужный API клиент
+def private_users_client(function_user) -> PrivateUsersClient:
+    """Фикстура приватного клиента пользователей с аутентификацией"""
+    return get_private_users_client(user=function_user.authentication_user)
+
+
+@pytest.fixture
 def function_user(public_users_client: PublicUsersClient) -> UserFixture:
-    request = CreateUserRequestSchema()
-    response = public_users_client.create_user(request)
-    return UserFixture(request=request, response=response)  # Возвращаем все нужные данные
+	request = CreateUserRequestSchema()
+	response = public_users_client.create_user(request)
+
+	auth_user = AuthenticationUserSchema(
+		email=request.email,
+		password=request.password
+	)
+
+	return UserFixture(request=request, response=response, authentication_user=auth_user)
--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema, \
+	UserSchema
 from tools.assertions.base import assert_equal
 
 
@@ -7,3 +8,22 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
 	assert_equal(response.user.last_name, request.last_name, "last_name")
 	assert_equal(response.user.first_name, request.first_name, "first_name")
 	assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """Проверяет корректность данных пользователя"""
+    assert_equal(actual.id, expected.id, "user_id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+
+def assert_get_user_response(
+    get_user_response: GetUserResponseSchema,
+    create_user_response: CreateUserResponseSchema
+):
+    """Проверяет, что данные пользователя при GET и CREATE совпадают"""
+    assert_user(
+        actual=get_user_response.user,
+        expected=create_user_response.user
+    )
