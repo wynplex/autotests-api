b79dcc691a5c98178ca20da9c8f8897c44f121f1
wynplex <wynplex@gmail.com>
2026-01-07 19:30:50 +0400

    Маркировки Pytest. Практика работы с pytest маркировками

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+import pytest
+
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.users.public_users_client import get_public_users_client
@@ -9,6 +11,8 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
 
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_login():
 	public_users_client = get_public_users_client()
 	authentication_client = get_authentication_client()
