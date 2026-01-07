b026b94e5be7c32f1e1c2915ee3500521453f4c7
wynplex <wynplex@gmail.com>
2026-01-07 22:19:46 +0400

    pytest basics

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,23 +1,22 @@
-import pytest
-
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from http import HTTPStatus
 
-from tools.assertions.base import assert_equal, assert_status_code
+import pytest
+
+from clients.users.public_users_client import PublicUsersClient
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
 
+
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user():
-	public_users_client = get_public_users_client()
+def test_create_user(public_users_client: PublicUsersClient):
+    request = CreateUserRequestSchema()
+    response = public_users_client.create_user_api(request)
+    response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
-	request = CreateUserRequestSchema()
-	response = public_users_client.create_user_api(request)
-	response_data = CreateUserResponseSchema.model_validate_json(response.text)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_create_user_response(request, response_data)
 
-	assert_status_code(response.status_code, HTTPStatus.OK)
-	assert_create_user_response(request, response_data)
-
-	validate_json_schema(instance=response.json(),schema=response_data.model_json_schema() )
+    validate_json_schema(response.json(), response_data.model_json_schema())
