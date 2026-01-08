a6f84eb4b726525e587988b44d1b989ea9272455
wynplex <wynplex@gmail.com>
2026-01-08 22:12:37 +0400

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -3,10 +3,12 @@ from http import HTTPStatus
 import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+	GetExerciseResponseSchema, GetExercisesQuerySchema
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -25,3 +27,23 @@ class TestExercises:
 		assert_status_code(response.status_code, HTTPStatus.OK)
 		assert_create_exercise_response(request, response_data)
 		validate_json_schema(response.json(), response_data.model_json_schema())
+
+	def test_get_exercise(
+			self,
+			exercises_client: ExercisesClient,
+			function_exercise: ExerciseFixture
+	):
+		"""Тест получения задания по ID через API"""
+		response = exercises_client.get_exercise_api(exercise_id=function_exercise.response.exercise.id)
+		response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+		assert_status_code(response.status_code, HTTPStatus.OK)
+		assert_get_exercise_response(
+			get_response=response_data,
+			create_response=function_exercise.response
+		)
+
+		validate_json_schema(
+			instance=response.json(),
+			schema=GetExerciseResponseSchema.model_json_schema()
+		)
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+	ExerciseSchema, GetExerciseResponseSchema, GetExercisesQuerySchema, GetExercisesResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -18,3 +19,36 @@ def assert_create_exercise_response(request: CreateExerciseRequestSchema,
 	assert_equal(response.exercise.order_index, request.order_index, "order_index")
 	assert_equal(response.exercise.description, request.description, "description")
 	assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+	"""
+	Проверяет, что фактические данные задания соответствуют ожидаемым.
+
+	:param actual: Фактические данные задания.
+	:param expected: Ожидаемые данные задания.
+	:return: AssertionError: Если хотя бы одно поле не совпадает
+	"""
+	assert_equal(actual.id, expected.id, "id")
+	assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+	assert_equal(actual.max_score, expected.max_score, "max_score")
+	assert_equal(actual.min_score, expected.min_score, "min_score")
+	assert_equal(actual.title, expected.title, "title")
+	assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+	assert_equal(actual.order_index, expected.order_index, "order_index")
+	assert_equal(actual.course_id, expected.course_id, "course_id")
+
+def assert_get_exercise_response(
+    get_response: GetExerciseResponseSchema,
+    create_response: CreateExerciseResponseSchema
+):
+    """
+    Проверяет, что данные задания при GET-запросе соответствуют данным при создании.
+
+    :param get_response: Ответ API на GET-запрос задания.
+    :param create_response: Ответ API на создание задания (из фикстуры).
+    :raises AssertionError: Если данные не совпадают.
+    """
+    assert_exercise(
+        actual=get_response.exercise,
+        expected=create_response.exercise
+    )
